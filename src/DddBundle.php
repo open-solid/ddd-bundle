39bab4a6e26b4c6cc066637683f7f0811e459c5b
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-10-26 12:30:44 -0400

    import services

--- src/DddBundle.php
+++ src/DddBundle.php
@@ -2,10 +2,17 @@
 
 namespace Yceruto\DddBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class DddBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+    }
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
